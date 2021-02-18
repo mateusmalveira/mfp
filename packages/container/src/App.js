aec71f363c2dda727b94391ef672de18fe077636
Mateus Malveira <mateusmalveira@protonmail.com>
2021-02-18 11:26:50 -0300

    add image

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,6 +5,11 @@ export default () => {
   return (
     <div>
       <h1>Oi</h1>
+      <img
+        src="https://i1.sndcdn.com/avatars-000316051482-v3sdrg-t500x500.jpg"
+        alt="Que delicia caraaa"
+        width="200"
+      />
       <hr />
       <MarketingApp />
     </div>
